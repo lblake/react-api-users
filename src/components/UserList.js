e22556958f803d6ac2a91e5d530285d66b81a161
Lloyd Blake <treleven.lloyd@gmail.com>
2021-07-31 17:18:23 +0100

    updated code to allow users to be displayed

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,30 +3,34 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 import '../css/userlist.css';
 
-export default function UserList({ data }) {
+export default function UserList() {
   const [users, setUsers] = useState([]);
-  const [filteredUser, setFilteredUser] = useState([...users]);
+  const [filteredUser, setFilteredUser] = useState([]);
 
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/users`).then((response) => {
       console.log(response.data);
       const newUsers = response.data;
       setUsers(newUsers);
+      setFilteredUser(newUsers);
     });
   }, []);
 
   const handleChange = (event) => {
     const searchWord = event.target.value;
-    console.log(searchWord);
     const newFilter = users.filter((value) => {
-      return value.name.toLowerCase().trim().indexOf(searchWord.toLowerCase().trim());
+      return (
+        value.name
+          .toLowerCase()
+          .trim()
+          .indexOf(searchWord.toLowerCase().trim()) > -1
+      );
     });
 
     if (searchWord === ' ') {
-      setFilteredUser([]);
+      setFilteredUser(users);
     } else {
       setFilteredUser(newFilter);
-      console.log('This is the new filter', newFilter);
     }
   };
 
@@ -47,7 +51,7 @@ export default function UserList({ data }) {
               </p>
               <div className='userInfo'>
                 <span className='textInfo'>
-                  Street:
+                  {/* Street:
                   <span className='addressInfo'> {user.address.street} </span>
                 </span>
                 <span className='textInfo'>
@@ -56,7 +60,7 @@ export default function UserList({ data }) {
                 </span>{' '}
                 <span className='textInfo'>
                   ZipCode:{' '}
-                  <span className='addressInfo'> {user.address.zipcode} </span>
+                  <span className='addressInfo'> {user.address.zipcode} </span> */}
                 </span>
               </div>
             </div>
