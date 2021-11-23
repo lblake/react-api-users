54d5cff7402218199c4c79ef3a361bd5f31ae3f2
lblake <treleven.lloyd@gmail.com>
2021-11-23 19:57:31 +0000

    updated code for test

--- src/components/UserListComponent/UserList.test.js
+++ src/components/UserListComponent/UserList.test.js
@@ -1,25 +1,17 @@
 import React from 'react';
-// import { createContext, useState, useEffect } from 'react';
-
-// import mockGetApiData from './getApiData.js';
 import { render, screen } from '@testing-library/react';
 import UserList from './UserList';
-// import UserContext from './UserList'
-
-// jest.mock('./getApiData.js');
-
-const users = [{ name: 'Mocked User', email: 'mocked@mocked.com' }];
-
+import { UserContext } from './UserContext';
 
 describe('when UserList component renders', () => {
-  // beforeEach(() => {
-  //   // const users = [{ name: 'Mocked User', email: 'mocked@mocked.com' }];
-  //   const resp = { data: users };
-  //   mockGetApiData.mockResolvedValue(resp);
-  // });
-
   it('should render UserList component', async () => {
-    render(<UserList.Provider value={users} />);
+    const mockedUsers = [{ name: 'Mocked User', email: 'mocked@mocked.com' }];
+
+    render(
+      <UserContext.Provider value={{ users: mockedUsers }}>
+        <UserList/>
+      </UserContext.Provider>
+    );
   });
   it('should test if word "User Information is on the page"', async () => {
     render(<UserList.Provider value={users} />);
